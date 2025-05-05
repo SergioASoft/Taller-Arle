7abec06cbd5fb34ac545b25adce879c098d5b01e
XDARKSLADERX <eddyhedy8@gmail.com>
2025-05-05 07:12:01 -0500

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,40 @@
+//Arreglo para Atención de clientes en supermercado
+let cola = []
+
+//Función para agregar clientes
+const agregarCliente = (nombre) => {
+    if (cola.length < 7) {
+        cola.push(nombre)
+        alert(` ${nombre} agregado con éxito.`)
+    } else {
+        alert("La fila esta llena (Máximo 7 clientes).")
+    }
+};
+
+//Función para atender clientes
+const atenderCliente = () => {
+    if (cola.length > 0) {
+        let cliente = cola.shift();
+        alert(`Atendiendo a: ${cliente}`);
+    } else {
+        alert("No hay clientes en la fila.");
+    }
+};
+
+//Menú principal
+while (true) {
+    let opcion = prompt("1. Agregar cliente\n2. Atender cliente\n3. Ver fila\n4. Dejar caja\nElige una opción:");
+    if (opcion === "1") {
+        let nombre = prompt("Ingresa el nombre del cliente:");
+        agregarCliente(nombre);
+    } else if (opcion === "2") {
+        atenderCliente()
+    } else if (opcion === "3") {
+        alert("Clientes en fila:\n" + (cola.length > 0 ? cola.join("\n"): "Ninguno"));
+    } else if (opcion === "4") {
+        alert("Dejando la caja...");
+        break;
+    } else {
+        alert("Opción invalida.")
+    }
+};
